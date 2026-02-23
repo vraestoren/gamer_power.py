3a49ff735e10ed9d3102d8b3f7a4eb75a8f30e09
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-23 15:47:03 +0300

    Update gamer_power.py

--- src/gamer_power.py
+++ src/gamer_power.py
@@ -1,57 +1,49 @@
-from requests import get
+from requests import Session
 
 class GamerPower:
-	def __init__(self) -> None:
-		self.api = "https://www.gamerpower.com/api"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
-		}
-	
-	def get_giveaways(self) -> dict:
-		return get(
-			f"{self.api}/giveaways",
-			headers=self.headers).json()
-	
-	def get_giveaways_by_platform(
-			self,
-			platform: str = "pc") -> dict:
-		return get(
-			f"{self.api}/giveaways?platform={platform}",
-			headers=self.headers).json()
-	
-	def get_giveaways_by_type(self, type: str = "game") -> dict:
-		return get(
-			f"{self.api}/giveaways?type={type}",
-			headers=self.headers).json()
-	
-	def sort_giveaways(self, sort: str) -> dict:
-		return get(
-			f"{self.api}/giveaways?sort-by={sort}",
-			headers=self.headers).json()
-	
-	def get_giveaways_by_all(
-			self,
-			platform: str = "steam",
-			type: str = "loot",
-			sort: str = "popularity") -> dict:
-		return get(
-			f"{self.api}/giveaways?platform={platform}&type={type}&sort-by={sort}",
-			headers=self.headers).json()
-		
-	def get_giveaway_details(self, giveaway_id: int) -> dict:
-		return get(
-			f"{self.api}/giveaway?id={giveaway_id}",
-			headers=self.headers).json()
-	
-	def filter_giveaway(
-			self,
-			platform: str = "epic-games-store.steam.android",
-			type: str = "game.loot") -> dict:
-		return get(
-			f"{self.api}/filter?platform={platform}&type={type}",
-			headers=self.headers).json()
-	
-	def get_total_giveaways_worth(self) -> dict:
-		return get(
-			f"{self.api}/worth",
-			headers=self.headers).json()
+    def __init__(self) -> None:
+        self.api = "https://www.gamerpower.com/api"
+        self.session = Session()
+        self.session.headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
+        }
+    
+    def get_giveaways(self) -> dict:
+        return self.session.get(
+            f"{self.api}/giveaways").json()
+    
+    def get_giveaways_by_platform(
+            self,
+            platform: str = "pc") -> dict:
+        return self.session.get(
+            f"{self.api}/giveaways?platform={platform}").json()
+    
+    def get_giveaways_by_type(self, type: str = "game") -> dict:
+        return self.session.get(
+            f"{self.api}/giveaways?type={type}").json()
+    
+    def sort_giveaways(self, sort: str) -> dict:
+        return self.session.get(
+            f"{self.api}/giveaways?sort-by={sort}").json()
+    
+    def get_giveaways_by_all(
+            self,
+            platform: str = "steam",
+            type: str = "loot",
+            sort: str = "popularity") -> dict:
+        return self.session.get(
+            f"{self.api}/giveaways?platform={platform}&type={type}&sort-by={sort}").json()
+        
+    def get_giveaway_details(self, giveaway_id: int) -> dict:
+        return self.session.get(
+            f"{self.api}/giveaway?id={giveaway_id}").json()
+    
+    def filter_giveaway(
+            self,
+            platform: str = "epic-games-store.steam.android",
+            type: str = "game.loot") -> dict:
+        return self.session.get(
+            f"{self.api}/filter?platform={platform}&type={type}").json()
+    
+    def get_total_giveaways_worth(self) -> dict:
+        return self.session.get(f"{self.api}/worth").json()
